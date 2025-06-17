d66822ed3e235c824a91498e31f7cc97e6d1dbac
nerfthisdev <barm2001@gmail.com>
2025-06-18 00:46:06 +0300

    feat: add logic to add new commands
    Also refactored the directory name because of potential conflict in
    names

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/nerthisdev/lab-q/internal/logger"
+	"github.com/nerthisdev/lab-q/internal/tgbot"
+)
+
 func main() {
+	logger := logger.GetLogger()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	token := os.Getenv("tg_api")
+
+	tgb := tgbot.Init(token, opts, "https://a3da-5-180-114-202.ngrok-free.app", ctx, &logger, ":2000")
+	tgb.Run(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
 }
