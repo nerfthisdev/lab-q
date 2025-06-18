53bf59cbee76d65caf936bd607679263eb7d6bf5
nerfthisdev <barm2001@gmail.com>
2025-06-18 11:02:37 +0300

    feat: bot setup with config

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"github.com/nerthisdev/lab-q/internal/config"
 	"github.com/nerthisdev/lab-q/internal/logger"
 	"github.com/nerthisdev/lab-q/internal/tgbot"
+	"go.uber.org/zap"
 )
 
 func main() {
+	configPath := flag.String("c", "./cmd/config.yaml", "path to go-telegram-bot-example config")
+
+	flag.Parse()
+
 	logger := logger.GetLogger()
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	cfg := &config.Config{}
+
+	err := config.GetConfiguration(*configPath, cfg)
+	if err != nil {
+		logger.Fatal("failed to get configuration", zap.String("reason", err.Error()))
+	}
 
 	defer cancel()
 
@@ -21,9 +35,7 @@ func main() {
 		bot.WithDefaultHandler(handler),
 	}
 
-	token := os.Getenv("tg_api")
-
-	tgb := tgbot.Init(token, opts, "https://a3da-5-180-114-202.ngrok-free.app", ctx, &logger, ":2000")
+	tgb := tgbot.Init(cfg, opts, ctx, &logger)
 	tgb.Run(ctx)
 }
 
--- internal/tgbot/bot.go
+++ internal/tgbot/bot.go
@@ -7,41 +7,29 @@ import (
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"github.com/nerthisdev/lab-q/internal/config"
 	"go.uber.org/zap"
 )
 
 type Tgbot struct {
-	Bot        *bot.Bot
-	Logger     *zap.Logger
-	port       string
-	webHookURL string
+	Bot    *bot.Bot
+	Logger *zap.Logger
+	Config *config.Config
 }
 
-func Init(apiKey string, opts []bot.Option, webHookURL string, ctx context.Context, logger *zap.Logger, port string) *Tgbot {
-	commands := []models.BotCommand{
-		{
-			Command:     "start",
-			Description: "launch the bot",
-		},
-		{
-			Command:     "register",
-			Description: "register in the bot",
-		},
-	}
-
+func Init(config *config.Config, opts []bot.Option, ctx context.Context, logger *zap.Logger) *Tgbot {
 	tgb := Tgbot{
-		Logger:     logger,
-		webHookURL: webHookURL,
+		Logger: logger,
+		Config: config,
 	}
 
-	b, err := bot.New(apiKey, opts...)
+	b, err := bot.New(tgb.Config.Bot.APIToken, opts...)
 	if err != nil {
 		tgb.Logger.Fatal("failed to create new bot instance", zap.String("error", err.Error()))
 	}
 	tgb.Logger.Info("created new bot instance")
 
 	tgb.Bot = b
-	tgb.registerCommands(ctx, commands)
 
 	button := bot.SetChatMenuButtonParams{
 		MenuButton: models.MenuButtonCommands{},
@@ -50,7 +38,7 @@ func Init(apiKey string, opts []bot.Option, webHookURL string, ctx context.Conte
 	tgb.Bot.SetChatMenuButton(ctx, &button)
 
 	set, err := tgb.Bot.SetWebhook(ctx, &bot.SetWebhookParams{
-		URL: tgb.webHookURL,
+		URL: tgb.Config.Bot.WebHookURL,
 	})
 	if err != nil {
 		tgb.Logger.Fatal("failed to set webhook", zap.String("error", err.Error()))
@@ -66,7 +54,7 @@ func Init(apiKey string, opts []bot.Option, webHookURL string, ctx context.Conte
 
 func (tgb *Tgbot) Run(ctx context.Context) {
 	go func() {
-		http.ListenAndServe(":2000", tgb.Bot.WebhookHandler())
+		http.ListenAndServe(tgb.Config.Server.Port, tgb.Bot.WebhookHandler())
 	}()
 
 	tgb.Bot.StartWebhook(ctx)
@@ -76,5 +64,4 @@ func (tgb *Tgbot) registerCommands(ctx context.Context, commands []models.BotCom
 	tgb.Bot.SetMyCommands(ctx, &bot.SetMyCommandsParams{
 		Commands: commands,
 	})
-
 }
